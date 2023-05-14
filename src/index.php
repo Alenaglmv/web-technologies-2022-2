114ef51bc9336a156a20ea3e8b73deb5b200002e
Alenaglmv <galimova.2002@mail.ru>
2023-05-14 13:56:20 +0500

    lesson 17

--- src/index.php
+++ src/index.php
@@ -1,33 +1,161 @@
 <?php
 
-$content = file_get_contents("./index.html");
+//1
+echo "ЗАДАНИЕ 1. ";
+$a = -2;
+$b = 5;
 
-$title = str_replace('{{ title }}', 'lesson16', $content);
-$h1 = str_replace('{{ h1 }}', 'Задание №16', $title);
-$thisYear = date('Y ') . 'год';
-$year = str_replace('{{ year }}', $thisYear, $h1);
-
-echo $year;
-
-$hours = date('H') + 2;
-$minutes = date('i');
-
-if($hours == 1 or $hours == 21) {
-    echo "$hours час ";
+if ($a >= 0 and $b >= 0) {
+    echo $a - $b . ' - разность';
 }
-else if(($hours >= 2 and $hours <= 4) or ($hours >= 22 and $hours <= 24)) {
-    echo "$hours часа ";
-}
-else if($hours >= 5 and $hours <= 20) {
-    echo "$hours часов ";
-}
-
-if($minutes == 1 or $minutes == 21 or $minutes == 31 or $minutes == 41 or $minutes == 51) {
-    echo "$minutes минута";
-}
-else if(($minutes >= 2 and $minutes <= 4) or ($minutes >= 22 and $minutes <= 24) or ($minutes >= 32 and $minutes <= 34) or ($minutes >= 42 and $minutes <= 44) or ($minutes >= 52 and $minutes <= 54)){
-    echo "$minutes минуты";
+elseif ($a < 0 and $b < 0) {
+    echo $a * $b . ' - произведение';
 }
 else {
-    echo "$minutes минут";
+    echo $a + $b . ' - сумма';
 }
+
+
+//2
+echo "<br>";
+echo "<br>ЗАДАНИЕ 2. ";
+
+$a = 7;
+switch ($a) {
+    case 0:
+        echo (0);
+    case 1:
+        echo (1) . ' ';
+    case 2:
+        echo (2) . ' ';
+    case 3:
+        echo (3) . ' ';
+    case 4:
+        echo (4) . ' ';
+    case 5:
+        echo (5) . ' ';
+    case 6:
+        echo (6) . ' ';
+    case 7:
+        echo (7) . ' ';
+    case 8:
+        echo (8) . ' ';
+    case 9:
+        echo (9) . ' ';
+    case 10:
+        echo (10) . ' ';
+    case 11:
+        echo (11) . ' ';
+    case 12:
+        echo (12) . ' ';
+    case 13:
+        echo (13) . ' ';
+    case 14:
+        echo (14) . ' ';
+    case 15:
+        echo (15) . ' ';
+}
+
+
+//3
+echo "<br>";
+echo "<br>ЗАДАНИЕ 3. ";
+
+$a = 8;
+$b = 2;
+
+function sum($a, $b) {
+    return $a + $b;
+}
+function dif($a, $b) {
+    return $a - $b;
+}
+function mul($a, $b) {
+    return $a * $b;
+}
+function div($a, $b) {
+    return $a / $b;
+}
+
+echo sum($a, $b) . ' - сумма, ' . dif($a, $b) .  ' - разность, ' . mul($a, $b) . ' - произведение, ' . div($a, $b) . ' - деление';
+
+
+//4
+echo "<br>";
+echo "<br>ЗАДАНИЕ 4. ";
+
+$a = 10;
+$b = 5;
+
+function mathOperation($a, $b, $operation) {
+    switch ($operation) {
+        case '+' :
+            return sum($a, $b);
+        case '-' :
+            return dif($a, $b);
+        case '*' :
+            return mul($a, $b);
+        case '/' :
+            return div($a, $b);
+    }
+}
+
+echo mathOperation($a, $b, '+') . ' - сумма, ' . mathOperation($a, $b, '-') .  ' - разность, ' . mathOperation($a, $b, '*') . ' - произведение, ' . mathOperation($a, $b, '/') . ' - деление';;
+
+
+//5
+echo "<br>";
+echo "<br>ЗАДАНИЕ 5. ";
+$thisYear = date('Y ') . 'год';
+
+//1способ
+$content = file_get_contents("./index.html");
+$year = str_replace('{{ year }}', $thisYear, $content);
+echo $year;
+
+//2способ
+echo $thisYear;
+
+//3способ
+?>
+    <!DOCTYPE html>
+    <html lang='ru'>
+
+    <head>
+        <meta charset='utf-8'>
+        <meta name='viewport' content='width=device-width, initial-scale=1, shrink-to-fit=no'>
+        <title></title>
+    </head>
+
+    <body>
+    <div>
+        <p><?= $thisYear?></p>
+    </div>
+    </body>
+
+    </html>
+<?php
+
+
+//6
+echo "ЗАДАНИЕ 6. ";
+
+$val = 4;
+$pow = 4;
+
+function power($val, $pow) {
+    if ($pow == 0 and $val != 0) {
+        return 1;
+    }
+    elseif ($pow < 0) {
+        return  1 / $val * power($val, $pow + 1);
+    }
+    elseif ($pow > 0) {
+        return $val * power($val, $pow - 1);
+    }
+    elseif ($val == 0) {
+        return 0;
+    }
+}
+
+echo power($val, $pow);
