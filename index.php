7fe3e534341909a587aab70f0291c54da7eb3b70
Alenaglmv <galimova.2002@mail.ru>
2023-06-01 21:49:31 +0500

    lesson 20

--- index.php
+++ index.php
@@ -1,100 +1,46 @@
 <?php
 
-//1 (статично объявлено в html)
-//echo file_get_contents('./index.html');
+include "./db.php";
 
-
-//2
-const DIR_IMG = './gallery';
-
-function renderTemplate($page, $arrImg = [], $arrHtml = []) {
-    ob_start();
-    include $page . ".php";
-    return ob_get_clean();
-}
-
-function renderArr() {
-    $arr = [];
-    $scanDir = scandir(DIR_IMG);
-    for ($i = 0; $i < count($scanDir); $i++) {
-        if (strlen($scanDir[$i]) > 2) {
-            $scanDirImg = scanDir(DIR_IMG . "/" . $scanDir[$i]);
-            for ($j = 0; $j < count($scanDirImg); $j++) {
-                if (strlen($scanDirImg[$j]) > 2) {
-                    $arr[$i - 2][$j - 2] = DIR_IMG . "/" . $scanDir[$i] . "/" . $scanDirImg[$j];
-                }
-            }
+function renderLesson($items, $nested) {
+    foreach ($items as $item) {
+        if ($item["nested"] == $nested) {
+            echo '<div class="list-item list-item_open" data-parent>' .
+                '<div class="list-item__inner">' .
+                '<img class="list-item__arrow" src="img/chevron-down.png" alt="" data-open>' .
+                '<img class="list-item__folder" src="img/folder2.png" alt="">';
+            echo $item["name"];
+            echo '</div>';
+            echo '<div class="list-item__items">';
+            renderLesson($items, $item["id"]);
+            echo '</div>';
+            echo '</div>';
         }
     }
-    return $arr;
 }
 
-$arrImg = renderArr();
-$gallery = renderTemplate('gallery', $arrImg);
-$arrHtml = array($gallery);
-
-echo renderTemplate('main', $arrImg, $arrHtml);
-
-
-//3
-if (!empty($_FILES)) {
-    $path = "gallery/small/" . $_FILES["myFile"]["name"];
-
-    if ($_FILES["myFile"]["size"] > 5*1024*1024) {
-        echo "Размер файла не должен превышать 5 мб";
-        exit;
-    }
-
-    //проверку на тип файла не делала, так как сразу указала расширение в input
-
-    if (move_uploaded_file($_FILES["myFile"]["tmp_name"], $path)) {
-        echo $message = "Файл загружен";
-        $image = imagecreatefromjpeg($path);
-        imagejpeg($image, "gallery/big/" . $_FILES["myFile"]["name"]);
-    } else {
-        echo $message = "Ошибка загрузки";
-    }
+function getLesson() {
+    return getAssocResult("SELECT * FROM `lesson`");
 }
 ?>
 
-    <!DOCTYPE html>
-    <html lang="ru">
+<html lang="ru">
 
-    <head>
-        <title></title>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-    </head>
+<head>
+    <meta charset="UTF-8">
+    <title>Каталог</title>
+    <link rel="stylesheet" href="style.css">
+</head>
 
-    <body>
-    <div class="add">
-        <form method="post" enctype="multipart/form-data">
-            <input id="file" type="file" name="myFile" accept="image/jpg, image/jpeg">
-            <input id="submit" type="submit" value="Загрузить">
-        </form>
-    </div>
-    </body>
+<body>
 
-    </html>
-<?php
+<div class="list-items" id="list-items">
+    <?=renderLesson(getLesson(), null)?>
+</div>
 
-//4и5
-$day = date('G:i:s d:m:Y');
+<script type="module" src="script.js"></script>
 
-function logging($day) {
-    if (file_exists("log.txt")) {
-        $countRecord = count(file("log.txt"));
-        if ($countRecord < 10) {
-            file_put_contents("log.txt", $day . PHP_EOL, FILE_APPEND);
-        }
-        else {
-            file_put_contents("countLog.txt", "Создан новый log файл," . PHP_EOL, FILE_APPEND);
-            $numberOfFileLog = count(file("countLog.txt"));
-            rename("log.txt", "log{$numberOfFileLog}.txt");
-        }
-    }
-    else {
-        file_put_contents('log.txt', $day . PHP_EOL, FILE_APPEND);
-    }
-}
-logging($day);
+</body>
+
+</html>
+
